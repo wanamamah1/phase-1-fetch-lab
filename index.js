573f60b9df776327ec6e138bee79983a846c39cf
aggrey wanamamah <aggreywanamamah008@gmail.com>
2025-04-18 18:08:02 +0100

    creates index.js

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(json => {
+      renderBooks(json);
+      return json; 
+    });
+}
